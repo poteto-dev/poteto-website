00730f74f65451d57f7d8bcc3207657ed71d8de8
Fariz <nizaralfariziakbar10@gmail.com>
2019-10-22 21:16:24 +0700

    add laravel stisla

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,6 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.sass('resources/sass/app.scss', 'public/css')
+    .js('resources/js/app.js', 'public/js').extract();
+mix.version();
