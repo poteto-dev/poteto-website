da7f646f844063d85059e70d8924d43a15b4721b
Fariz <nizaralfariziakbar10@gmail.com>
2019-10-22 23:40:36 +0700

    fix conflict

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,7 +14,6 @@ const mix = require('laravel-mix');
 mix.sass('resources/sass/app.scss', 'public/css')
     .js('resources/js/app.js', 'public/js').extract();
 mix.version();
-mix.js('resources/js/app.js', 'public/js');
 mix.copy('node_modules/bootstrap/dist/js/bootstrap.js', 'public/js/bootstrap.js');
 mix.copy('node_modules/jquery/dist/jquery.js', 'public/js/jquery.js');
 mix.sass('resources/sass/style.scss', 'public/css');
