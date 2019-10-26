4273221df84e18ed4f7a68e18e4b5cecd0caa451
arifinizzah <arifin.izzah15@gmail.com>
2019-10-26 15:54:35 +0700

    edit "Omni Potato" to "Omni Poteto"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="shortcut icon" type="image/png" href="{{ asset("img/poteto_favicon.png") }}"/>
 
-  <title>Omni Potato - @yield('title')</title>
+  <title>Omni Poteto - @yield('title')</title>
 
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
   <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
