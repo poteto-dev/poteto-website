e4784fba5023c10d0b6d63b5b38e91cc8566a876
arifinizzah <arifin.izzah15@gmail.com>
2019-10-29 02:03:09 +0700

    add section separator & section 2

--- resources/views/_home.blade.php
+++ resources/views/_home.blade.php
@@ -10,6 +10,9 @@
           <img src="{{ asset('img/poteto_favicon.png') }}" class="undraggable d-inline-block align-top" alt="">
           <span class="navbar-title display-4 ml-md-2">Omni Poteto</span>
         </a>
+        <a href="https://github.com/poteto-dev">
+          <img src="{{ asset('assets/img/github.svg') }}" class="undraggable ml-auto github-svg d-none d-md-block" width="40" height="40" alt="">
+        </a>
       </nav>
       <div class="row section-1__row">
         <div class="col-md-6">
@@ -27,6 +30,27 @@
         <div class="col-md-6">
           <div class="card card-body border-0">
             <img class="undraggable" src="{{ asset("assets/img/section-1__2.svg") }}" width="518" height="360">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <img class="undraggable wave" src="{{ asset("assets/img/wave_1-2.svg") }}">
+
+  <section class="section-2">
+    <div class="container">
+      <div class="row h-100">
+        <div class="col-md-6 h-100">
+          <div class="card card-body bg-transparent border-0 h-100">
+            <img class="undraggable section-2__img" src="{{ asset("assets/img/section-2.svg") }}" width="518" height="360">
+          </div>
+        </div>
+        <div class="col-md-6 h-100">
+          <div class="card card-body bg-transparent border-0 h-100">
+            <div class="my-auto">
+              <h1 class="font-bb">What can we do?</h1>
+            </div>
           </div>
         </div>
       </div>
