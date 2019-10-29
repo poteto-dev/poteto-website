35ccc38be4a43dce7bae7d0a0da3a575101d81a4
arifinizzah <arifin.izzah15@gmail.com>
2019-10-29 15:26:46 +0700

    add some style

--- resources/views/_home.blade.php
+++ resources/views/_home.blade.php
@@ -11,7 +11,7 @@
           <span class="navbar-title display-4 ml-md-2">Omni Poteto</span>
         </a>
         <a href="https://github.com/poteto-dev">
-          <img src="{{ asset('assets/img/github.svg') }}" class="undraggable ml-auto github-svg d-none d-md-block" width="40" height="40" alt="">
+          <img src="{{ asset('svg/icon/github.svg') }}" class="undraggable ml-auto github-svg d-none d-md-block" width="40" height="40" alt="">
         </a>
       </nav>
       <div class="row section-1__row">
@@ -29,27 +29,48 @@
         </div>
         <div class="col-md-6">
           <div class="card card-body border-0">
-            <img class="undraggable" src="{{ asset("assets/img/section-1__2.svg") }}" width="518" height="360">
+            <img class="undraggable" src="{{ asset("svg/section/section-1.svg") }}" width="518" height="360">
           </div>
         </div>
       </div>
     </div>
   </section>
 
-  <img class="undraggable wave" src="{{ asset("assets/img/wave_1-2.svg") }}">
+  <img class="undraggable wave" src="{{ asset("svg/section/separator_1-2.svg") }}">
 
   <section class="section-2">
     <div class="container">
       <div class="row h-100">
         <div class="col-md-6 h-100">
           <div class="card card-body bg-transparent border-0 h-100">
-            <img class="undraggable section-2__img" src="{{ asset("assets/img/section-2.svg") }}" width="518" height="360">
+            <img class="undraggable section-2__img" src="{{ asset("svg/section/section-2.svg") }}" width="518" height="360">
           </div>
         </div>
         <div class="col-md-6 h-100">
           <div class="card card-body bg-transparent border-0 h-100">
             <div class="my-auto">
-              <h1 class="font-bb">What can we do?</h1>
+              <h1 class="font-bb">Our Expertise</h1>
+              <p class="font-br ls-03 mt-3">Basically, we use the best programming tools and languages for the development process.</p>
+              <p class="font-bb ls-03">Languages:</p>
+              <div class="align-items-center justify-content-start">
+                <img class="lang-icon mr-2" src="{{ asset("svg/icon/lang/kotlin.svg") }}" data-toggle="tooltip" title="kotlin">
+                <img class="lang-icon mr-2" src="{{ asset("svg/icon/lang/dart.svg") }}" data-toggle="tooltip" title="dart">
+                <img class="lang-icon mr-2" src="{{ asset("svg/icon/lang/java.svg") }}" data-toggle="tooltip" title="java">
+                <img class="lang-icon mr-2" src="{{ asset("svg/icon/lang/php.svg") }}" style="height: 25px" data-toggle="tooltip" title="php">
+                <img class="lang-icon mr-2" src="{{ asset("svg/icon/lang/sass.svg") }}" data-toggle="tooltip" title="sass">
+                <img class="lang-icon mr-2" src="{{ asset("svg/icon/lang/javascript.svg") }}" data-toggle="tooltip" title="javascript">
+                <img class="lang-icon mr-2" src="{{ asset("svg/icon/lang/typescript.svg") }}" data-toggle="tooltip" title="typescript">
+              </div>
+              <p class="font-bb ls-03 mt-3">Framework & libraries:</p>
+              <div class="align-items-center justify-content-start">
+                <img class="lang-icon mr-2" src="{{ asset("svg/icon/lib/vue.svg") }}" data-toggle="tooltip" title="vue">
+                <img class="lang-icon mr-2" src="{{ asset("svg/icon/lib/laravel.svg") }}" data-toggle="tooltip" title="laravel">
+                <img class="lang-icon mr-2" src="{{ asset("svg/icon/lib/flutter.svg") }}" data-toggle="tooltip" title="flutter">
+                <img class="lang-icon mr-2" src="{{ asset("svg/icon/lib/clojure.svg") }}" data-toggle="tooltip" title="clojure">
+                <img class="lang-icon mr-2" src="{{ asset("svg/icon/lib/bootstrap.svg") }}" data-toggle="tooltip" title="bootstrap">
+                <img class="lang-icon mr-2" src="{{ asset("svg/icon/lib/jquery.svg") }}" style="width: 100px;" data-toggle="tooltip" title="typescript">
+              </div>
+              <p class="font-br ls-03 mt-4">Besides programming, we also have expertise in design.</p>
             </div>
           </div>
         </div>
@@ -57,10 +78,87 @@
     </div>
   </section>
 
+  <img class="undraggable wave" src="{{ asset("svg/section/separator_2-3.svg") }}">
+
+  <section class="section-3">
+    <div class="container">
+
+    </div>
+  </section>
+
 @endsection
 
 @section('javascript')
   <script>
+      $( document ).ready(function() {
+          $('img[title="kotlin"]').tooltip({
+              animated: 'fade',
+              placement: 'top',
+              html: true
+          });
+          $('img[title="dart"]').tooltip({
+              animated: 'fade',
+              placement: 'top',
+              html: true
+          });
+          $('img[title="java"]').tooltip({
+              animated: 'fade',
+              placement: 'top',
+              html: true
+          });
+          $('img[title="php"]').tooltip({
+              animated: 'fade',
+              placement: 'top',
+              html: true
+          });
+          $('img[title="sass"]').tooltip({
+              animated: 'fade',
+              placement: 'top',
+              html: true
+          });
+          $('img[title="javascript"]').tooltip({
+              animated: 'fade',
+              placement: 'top',
+              html: true
+          });
+          $('img[title="typescript"]').tooltip({
+              animated: 'fade',
+              placement: 'top',
+              html: true
+          });
+
+          $('img[title="vue"]').tooltip({
+              animated: 'fade',
+              placement: 'top',
+              html: true
+          });
+          $('img[title="laravel"]').tooltip({
+              animated: 'fade',
+              placement: 'top',
+              html: true
+          });
+          $('img[title="flutter"]').tooltip({
+              animated: 'fade',
+              placement: 'top',
+              html: true
+          });
+          $('img[title="clojure"]').tooltip({
+              animated: 'fade',
+              placement: 'top',
+              html: true
+          });
+          $('img[title="bootstrap"]').tooltip({
+              animated: 'fade',
+              placement: 'top',
+              html: true
+          });
+          $('img[title="jquery"]').tooltip({
+              animated: 'fade',
+              placement: 'top',
+              html: true
+          });
+      });
+
     var docWidth = document.documentElement.offsetWidth;
     [].forEach.call(
       document.querySelectorAll('*'),
