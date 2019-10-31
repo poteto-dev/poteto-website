a9ffb6f96e535df546949e88258a84ce7365068c
arifinizzah <arifin.izzah15@gmail.com>
2019-10-31 22:21:22 +0700

    add aos for animation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,7 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
   <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
   <link rel="stylesheet" href="{{ asset('css/animate.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/aos.css') }}">
   <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
 </head>
@@ -19,6 +20,7 @@
   <script src="{{ asset('js/jquery.js') }}"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
   <script src="{{ asset('js/bootstrap.js') }}"></script>
+  <script src="{{ asset('js/aos.js') }}"></script>
 
   @yield('javascript')
 </body>
